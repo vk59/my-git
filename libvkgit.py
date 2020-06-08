7599a0f0bf88e2c150e73b0bf037ea2ee492060f
ivan <jkostylev592@gmail.com>
2020-06-08 15:48:55 +0300

    Implemented creating and reading a blob-object

--- libvkgit.py
+++ libvkgit.py
@@ -1,15 +1,19 @@
 import argparse
 import collections
 import configparser
+import hashlib
 import os
 import re
 import sys
-import zlib
+import zlib 
 
 argparser = argparse.ArgumentParser(description='Content Tracker')
 argsubparsers = argparser.add_subparsers(title='Commands', dest='command')
 argsubparsers.required = True
 
+# Subparsers
+
+# subparser init
 argsp = argsubparsers.add_parser("init", help="Initialize a new, empty repository.")
 
 argsp.add_argument("path",
@@ -18,9 +22,38 @@ argsp.add_argument("path",
                    default=".",
                    help="Where to create the repository.")
 
+# subparser cat-file
+argsp = argsubparsers.add_parser("cat-file",
+                                 help="Provide content of repository objects")
 
-def cmd_init(args):
-    repo_create(args.path)
+argsp.add_argument("type",
+                   metavar="type",
+                   choices=["blob", "commit", "tag", "tree"],
+                   help="Specify the type")
+
+argsp.add_argument("object",
+                   metavar="object",
+                   help="The object to display")
+
+# subparser hash-object
+argsp = argsubparsers.add_parser(
+    "hash-object",
+    help="Compute object ID and optionally creates a blob from a file")
+
+argsp.add_argument("-t",
+                   metavar="type",
+                   dest="type",
+                   choices=["blob", "commit", "tag", "tree"],
+                   default="blob",
+                   help="Specify the type")
+
+argsp.add_argument("-w",
+                   dest="write",
+                   action="store_true",
+                   help="Actually write the object into the database")
+
+argsp.add_argument("path",
+                   help="Read object from <file>")
 
 
 class GitRepository(object):
@@ -51,6 +84,34 @@ class GitRepository(object):
             if vers != 0:
                 raise Exception("Unsupported repositoryformatversion %s" % vers)
     
+
+class GitObject (object):
+
+    repo = None
+
+    def __init__(self, repo, data=None):
+        self.repo=repo
+
+        if data != None:
+            self.deserialize(data)
+
+    def serialize(self):
+        """This function MUST be implemented by subclasses."""
+        raise Exception("Unimplemented!")
+
+    def deserialize(self, data):
+        raise Exception("Unimplemented!")
+
+
+class GitBlob(GitObject):
+    fmt=b'blob'
+
+    def serialize(self):
+        return self.blobdata
+
+    def deserialize(self, data):
+        self.blobdata = data
+
 
 def repo_path(repo, *pathargs):
     """Compute path under repo's gitdir."""
@@ -146,20 +207,110 @@ def repo_find(path=".", required=True):
     return repo_find(parent, required)
 
 
+def object_read(repo, sha):
+    """Read object object_id from Git repository repo.  Return a
+    GitObject whose exact type depends on the object."""
+
+    path = repo_file(repo, "objects", sha[0:2], sha[2:])
+
+    with open (path, "rb") as f:
+        raw = zlib.decompress(f.read())
+
+        # Read object type
+        x = raw.find(b' ')
+        fmt = raw[0:x]
+
+        # Read and validate object size
+        y = raw.find(b'\x00', x)
+        size = int(raw[x:y].decode("ascii"))
+        if size != len(raw)-y-1:
+            raise Exception("Malformed object {0}: bad length".format(sha))
+
+        # Pick constructor
+        # if   fmt==b'commit' : c=GitCommit
+        # elif fmt==b'tree'   : c=GitTree
+        # elif fmt==b'tag'    : c=GitTag
+        if fmt==b'blob'   : c=GitBlob
+        else:
+            raise Exception("Unknown type {} for object {}".format(fmt.decode("ascii"), sha))
+
+        # Call constructor and return object
+        return c(repo, raw[y+1:])
+
+
+def object_find(repo, name, fmt=None, follow=True):
+    return name
+
+
+def object_write(obj, actually_write=True):
+    data = obj.serialize()
+    result = obj.fmt + b' ' + str(len(data)).encode() + b'\x00' + data
+    sha = hashlib.sha1(result).hexdigest()
+
+    if actually_write:
+        # Compute path
+        path = repo_file(obj.repo, "objects", sha[0:2], sha[2:], mkdir=actually_write)
+
+        with open(path, 'wb') as f:
+            # Compress and write
+            f.write(zlib.compress(result))
+
+    return sha
+
+
+def object_hash(fd, fmt, repo=None):
+    data = fd.read()
+
+    # Choose constructor depending on
+    # object type found in header.
+    if fmt==b'blob'   : obj=GitBlob(repo, data)
+    # elif   fmt==b'commit' : obj=GitCommit(repo, data)
+    # elif fmt==b'tree'   : obj=GitTree(repo, data)
+    # elif fmt==b'tag'    : obj=GitTag(repo, data)
+    
+    else:
+        raise Exception("Unknown type %s!" % fmt)
+
+    return object_write(obj, repo)
+
+
+# COMMANDS
+def cmd_init(args):
+    repo_create(args.path)
+
+def cmd_cat_file(args):
+    repo = repo_find()
+    cat_file(repo, args.object, fmt=args.type.encode())
+
+def cat_file(repo, obj, fmt=None):
+    obj = object_read(repo, object_find(repo, obj, fmt=fmt))
+    sys.stdout.buffer.write(obj.serialize())
+
+def cmd_hash_object(args):
+    if args.write:
+        repo = GitRepository(".")
+    else:
+        repo = None
+
+    with open(args.path, "rb") as fd:
+        sha = object_hash(fd, args.type.encode(), repo)
+        print(sha)
+
+
 def main(argv=sys.argv[1:]):
     args = argparser.parse_args(argv)
 
-    if   args.command == "add"         : cmd_add(args)
-    elif args.command == "cat-file"    : cmd_cat_file(args)
-    elif args.command == "checkout"    : cmd_checkout(args)
-    elif args.command == "commit"      : cmd_commit(args)
+    # if   args.command == "add"         : cmd_add(args)
+    if args.command == "cat-file"    : cmd_cat_file(args)
+    # elif args.command == "checkout"    : cmd_checkout(args)
+    # elif args.command == "commit"      : cmd_commit(args)
     elif args.command == "hash-object" : cmd_hash_object(args)
     elif args.command == "init"        : cmd_init(args)
-    elif args.command == "log"         : cmd_log(args)
-    elif args.command == "ls-tree"     : cmd_ls_tree(args)
-    elif args.command == "merge"       : cmd_merge(args)
-    elif args.command == "rebase"      : cmd_rebase(args)
-    elif args.command == "rev-parse"   : cmd_rev_parse(args)
-    elif args.command == "rm"          : cmd_rm(args)
-    elif args.command == "show-ref"    : cmd_show_ref(args)
-    elif args.command == "tag"         : cmd_tag(args)
+    # elif args.command == "log"         : cmd_log(args)
+    # elif args.command == "ls-tree"     : cmd_ls_tree(args)
+    # elif args.command == "merge"       : cmd_merge(args)
+    # elif args.command == "rebase"      : cmd_rebase(args)
+    # elif args.command == "rev-parse"   : cmd_rev_parse(args)
+    # elif args.command == "rm"          : cmd_rm(args)
+    # elif args.command == "show-ref"    : cmd_show_ref(args)
+    # elif args.command == "tag"         : cmd_tag(args)
