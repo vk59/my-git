b85412b109b878798e3a7d4b8fe6381a8965d013
ivan <jkostylev592@gmail.com>
2020-06-08 20:11:48 +0300

    Implemented reading commit data (checkout)

--- libvkgit.py
+++ libvkgit.py
@@ -63,6 +63,21 @@ argsp.add_argument("commit",
                    nargs="?",
                    help="Commit to start at.")
 
+# subparser ls-tree
+argsp = argsubparsers.add_parser("ls-tree", help="Pretty-print a tree object.")
+
+argsp.add_argument("object",
+                   help="The object to show.")
+
+# subparser checkout
+argsp = argsubparsers.add_parser("checkout", help="Checkout a commit inside of a directory.")
+
+argsp.add_argument("commit",
+                   help="The commit or tree to checkout.")
+
+argsp.add_argument("path",
+                   help="The EMPTY directory to checkout on.")
+
 
 class GitRepository(object):
     """ Git-repo """
@@ -122,13 +137,30 @@ class GitBlob(GitObject):
 
 
 class GitCommit(GitObject):
-    fmt=b'commit'
+    fmt=b'commit'   
 
     def deserialize(self, data):
         self.kvlm = kvlm_parse(data)
 
     def serialize(self):
         return kvlm_serialize(self.kvlm)
+
+
+class GitTreeLeaf(object):
+    def __init__(self, mode, path, sha):
+        self.mode = mode
+        self.path = path
+        self.sha = sha
+
+
+class GitTree(GitObject):
+    fmt=b'tree'
+
+    def deserialize(self, data):
+        self.items = tree_parse(data)
+
+    def serialize(self):
+        return tree_serialize(self)
 
 
 def repo_path(repo, *pathargs):
@@ -244,10 +276,10 @@ def object_read(repo, sha):
             raise Exception("Malformed object {0}: bad length".format(sha))
 
         # Pick constructor
-        # if   fmt==b'commit' : c=GitCommit
+        if   fmt==b'commit' : c=GitCommit
         # elif fmt==b'tree'   : c=GitTree
         # elif fmt==b'tag'    : c=GitTag
-        if fmt==b'blob'   : c=GitBlob
+        elif fmt==b'blob'   : c=GitBlob
         else:
             raise Exception("Unknown type {} for object {}".format(fmt.decode("ascii"), sha))
 
@@ -281,8 +313,8 @@ def object_hash(fd, fmt, repo=None):
     # Choose constructor depending on
     # object type found in header.
     if fmt==b'blob'   : obj=GitBlob(repo, data)
-    # elif   fmt==b'commit' : obj=GitCommit(repo, data)
-    # elif fmt==b'tree'   : obj=GitTree(repo, data)
+    elif   fmt==b'commit' : obj=GitCommit(repo, data)
+    elif fmt==b'tree'   : obj=GitTree(repo, data)
     # elif fmt==b'tag'    : obj=GitTag(repo, data)
     
     else:
@@ -363,35 +395,6 @@ def kvlm_serialize(kvlm):
     return ret
 
 
-# COMMANDS
-def cmd_init(args):
-    repo_create(args.path)
-
-def cmd_cat_file(args):
-    repo = repo_find()
-    cat_file(repo, args.object, fmt=args.type.encode())
-
-def cat_file(repo, obj, fmt=None):
-    obj = object_read(repo, object_find(repo, obj, fmt=fmt))
-    sys.stdout.buffer.write(obj.serialize())
-
-def cmd_hash_object(args):
-    if args.write:
-        repo = GitRepository(".")
-    else:
-        repo = None
-
-    with open(args.path, "rb") as fd:
-        sha = object_hash(fd, args.type.encode(), repo)
-        print(sha)
-
-def cmd_log(args):
-    repo = repo_find()
-
-    print("digraph wyaglog{")
-    log_graphviz(repo, object_find(repo, args.commit), set())
-    print("}")
-
 def log_graphviz(repo, sha, seen):
 
     if sha in seen:
@@ -415,17 +418,146 @@ def log_graphviz(repo, sha, seen):
         print ("c_{0} -> c_{1};".format(sha, p))
         log_graphviz(repo, p, seen)
 
+
+def tree_parse_one(raw, start=0):
+    # Find the space terminator of the mode
+    x = raw.find(b' ', start)
+    assert(x-start == 5 or x-start==6)
+
+    # Read the mode
+    mode = raw[start:x]
+
+    # Find the NULL terminator of the path
+    y = raw.find(b'\x00', x)
+    # and read the path
+    path = raw[x+1:y]
+
+    # Read the SHA and convert to an hex string
+    sha = hex(
+        int.from_bytes(
+            raw[y+1:y+21], "big"))[2:] # hex() adds 0x in front,
+                                           # we don't want that.
+    return y+21, GitTreeLeaf(mode, path, sha)
+
+
+def tree_parse(raw):
+    pos = 0
+    max = len(raw)
+    ret = list()
+    while pos < max:
+        pos, data = tree_parse_one(raw, pos)
+        ret.append(data)
+
+    return ret
+
+
+def tree_serialize(obj):
+    #@FIXME Add serializer!
+    ret = b''
+    for i in obj.items:
+        ret += i.mode
+        ret += b' '
+        ret += i.path
+        ret += b'\x00'
+        sha = int(i.sha, 16)
+        # @FIXME Does
+        ret += sha.to_bytes(20, byteorder="big")
+    return ret
+
+
+def tree_checkout(repo, tree, path):
+    for item in tree.items:
+        obj = object_read(repo, item.sha)
+        dest = os.path.join(path, item.path)
+
+        if obj.fmt == b'tree':
+            os.mkdir(dest)
+            tree_checkout(repo, obj, dest)
+        elif obj.fmt == b'blob':
+            with open(dest, 'wb') as f:
+                f.write(obj.blobdata)
+
+
+# COMMANDS
+def cmd_init(args):
+    repo_create(args.path)
+
+
+def cmd_cat_file(args):
+    repo = repo_find()
+    cat_file(repo, args.object, fmt=args.type.encode())
+
+
+def cat_file(repo, obj, fmt=None):
+    obj = object_read(repo, object_find(repo, obj, fmt=fmt))
+    sys.stdout.buffer.write(obj.serialize())
+
+
+def cmd_hash_object(args):
+    if args.write:
+        repo = GitRepository(".")
+    else:
+        repo = None
+
+    with open(args.path, "rb") as fd:
+        sha = object_hash(fd, args.type.encode(), repo)
+        print(sha)
+
+
+def cmd_log(args):
+    repo = repo_find()
+
+    print("digraph wyaglog{")
+    log_graphviz(repo, object_find(repo, args.commit), set())
+    print("}")
+
+
+def cmd_ls_tree(args):
+    repo = repo_find()
+    obj = object_read(repo, object_find(repo, args.object, fmt=b'tree'))
+
+    for item in obj.items:
+        print("{0} {1} {2}\t{3}".format(
+            "0" * (6 - len(item.mode)) + item.mode.decode("ascii"),
+            # Git's ls-tree displays the type
+            # of the object pointed to.  We can do that too :)
+            object_read(repo, item.sha).fmt.decode("ascii"),
+            item.sha,
+            item.path.decode("ascii")))
+
+
+def cmd_checkout(args):
+    repo = repo_find()
+
+    obj = object_read(repo, object_find(repo, args.commit))
+
+    # If the object is a commit, we grab its tree
+    if obj.fmt == b'commit':
+        obj = object_read(repo, obj.kvlm[b'tree'].decode("ascii"))
+
+    # Verify that path is an empty directory
+    if os.path.exists(args.path):
+        if not os.path.isdir(args.path):
+            raise Exception("Not a directory {0}!".format(args.path))
+        if os.listdir(args.path):
+            raise Exception("Not empty {0}!".format(args.path))
+    else:
+        os.makedirs(args.path)
+
+    tree_checkout(repo, obj, os.path.realpath(args.path).encode()) 
+
+
 def main(argv=sys.argv[1:]):
     args = argparser.parse_args(argv)
 
     # if   args.command == "add"         : cmd_add(args)
     if args.command == "cat-file"    : cmd_cat_file(args)
-    # elif args.command == "checkout"    : cmd_checkout(args)
+    elif args.command == "checkout"    : cmd_checkout(args)
     # elif args.command == "commit"      : cmd_commit(args)
     elif args.command == "hash-object" : cmd_hash_object(args)
     elif args.command == "init"        : cmd_init(args)
-    # elif args.command == "log"         : cmd_log(args)
-    # elif args.command == "ls-tree"     : cmd_ls_tree(args)
+    elif args.command == "log"         : cmd_log(args)
+    elif args.command == "ls-tree"     : cmd_ls_tree(args)
     # elif args.command == "merge"       : cmd_merge(args)
     # elif args.command == "rebase"      : cmd_rebase(args)
     # elif args.command == "rev-parse"   : cmd_rev_parse(args)
